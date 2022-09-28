41a2d7f2b29785a183685ad0b036e9b1f8f641ce
Anna Berezuk <an.berezuk@gmail.com>
2022-09-29 00:16:45 +0300

    ДЗ3 для семминара 3 (языки программирования)

---
+++ sem03DZ/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.Clear();
+int[] num = {1,2,3,4,5};
---
+++ sem3/Program.cs
@@ -0,0 +1,37 @@
+﻿//Console.Clear();
+
+//способы задать массив
+//int[] num = new int[5];
+//int[] num = new int[5]{1,2,3,4,5};
+//int[] num = [] {1,2,3,4,5};
+//int[] num = new int[]{1,2,3,4,5};
+// int[] num = {1,2,3,4,5};
+
+// string[] names = {"Ann","John","Bob", "Liz"};
+
+// Console.WriteLine(names[names.Length-1]);
+// names[names.Length-1]="Mark";
+// Console.WriteLine(names[names.Length-1]);
+
+// for (int i = 0; i < names.Length; i++)
+//     Console.WriteLine(names[i]);
+//Math.Abs - модуль числа (абсолют числа)
+//Math.Pow - степень числа (абсолют числа)
+//Math.Sqrt(x) - возведение в квадрат
+
+//double x = 9.123456;
+//double y = Math.Round(x,2);
+//double y = Math.Round(x);
+//double y = Math.(x);
+//Console.WriteLine(y);
+
+//принимает на вход координаты  точки (x и y) ,  при этом x и y не равны 0 и выдает номер четверти плоскости, в которой находится эта точка.
+
+// Console.Clear();
+// Console.Write("Введите число x: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if((x==0) || (y==0)) Console.Write("Введите числа не равные 0");
+// if((x > 0) & (y > 0))
+// Console.WriteLine("1");
